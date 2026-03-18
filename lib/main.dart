75ba74448e6d3a2d56da68794cc1960281782568
Karola <karolina16478@gmail.com>
2026-03-18 13:27:50 +0100

    added solid_lints

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
+/// Entry point.
 void main() {
-  runApp(const MyApp());
+  runApp(const Main());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// Default [Main] class.
+class Main extends StatelessWidget {
+  /// [Main] Constructor.
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Test Task',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       )
