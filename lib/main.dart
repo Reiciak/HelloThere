732e5eef3cb28d191fe7a357a9289fa45a4b62f1
Karola <karolina16478@gmail.com>
2026-03-25 20:19:32 +0100

    style: improve main code style

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,17 @@ import 'package:hello_there/pages/main_page.dart';
 void main() {
   runApp(const Main());
 }
+
 /// Default [Main] class.
 class Main extends StatelessWidget {
+
   /// [Main] Constructor.
   const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Test Task',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-        home: const MainPage(),
+    return const MaterialApp(
+        home: MainPage(),
     );
   }
 }
